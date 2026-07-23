f509666b32c2d359f2ccd4ac2624f8968fdfb58b
owwwy <owanley@yandex.ru>
2026-07-23 17:22:30 +0300

    Move projects to data file and reset tracking

---
+++ src/data/projects.ts
@@ -0,0 +1,14 @@
+export interface Project {
+	title: string;
+	category: string;
+	image?: string;
+	imageAlt?: string;
+}
+
+export const projects: Project[] = [
+	{
+		title: 'ADDINOL',
+		category: 'e-commerce',
+		imageAlt: 'Проект ADDINOL',
+	},
+];
